d25b5ff805abbbf01974dfa74d63658570bf3fd4
Agustin <agustintour.3@gmail.com>
2024-08-15 12:37:40 -0300

    movimiento hecho, obstaculo y su funcion

--- scenes/Game.js
+++ scenes/Game.js
@@ -15,19 +15,26 @@ export default class Game extends Phaser.Scene {
 
   create() {
      //Pala
-     this.rectangle = this.add.rectangle(400, 550, 140, 30, 0xffffff);
+     this.rectangle = this.add.rectangle(400, 500, 140, 30, 0xffffff);
      //Pelota
-     this.ball = this.add.circle(200,300,20,0xffffff);
- 
+     this.ball = this.add.circle(300,400,20,0xffffff);
+    //Fisicas pala
      this.physics.add.existing(this.rectangle);
      this.rectangle.body.setImmovable(true);
      this.rectangle.body.setCollideWorldBounds(true);
+     //fisicas bola
      this.physics.add.existing(this.ball);
      this.ball.body.setCollideWorldBounds(true);
      this.ball.body.setBounce(1);
-     this.ball.body.setVelocity(200, 200)
+     this.ball.body.setVelocity(450, 300);
+     // Obstaculo
+     this.obstacle = this.add.rectangle(300,200,140,100,0xffffff),
+     this.physics.add.existing(this.obstacle);
+     this.obstacle.body.setImmovable(true);
+     this.obstacle.body.setBounce(1);
+     //Creacion de teclas de movimiento
      this.cursor = this.input.keyboard.createCursorKeys();
-
+    //Colliders 
      this.physics.add.collider(
       this.rectangle,
       this.ball,
@@ -35,17 +42,31 @@ export default class Game extends Phaser.Scene {
       null,
       this
     )
+   this.physics.add.collider(
+    this.ball,
+    this.obstacle,
+    this.handleCollision = (ball, obstacle) => {
+      console.log("colision");
+      obstacle.destroy();
+    },
+    null,
+    this
+   )
+  
   }
 
   update() {
     //Movimiento pala
-    const speed = 500;
+    /*const speed = 500;
     this.rectangle.body.setVelocity(0);
         if (this.cursor.left.isDown) {
             this.rectangle.body.setVelocityX(-speed);
         } else if (this.cursor.right.isDown) {
             this.rectangle.body.setVelocityX(speed);
         }
-        //Movimiento pelota
+        */
+        this.input.on('pointermove', (pointer) => {
+          this.rectangle.x = pointer.x;
+      });
   }
 }
