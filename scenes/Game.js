a66c77f1408a6d6bdde395930c026bc1c7939f0c
Agustin <agustintour.3@gmail.com>
2024-08-15 12:14:00 -0300

    Movimiento bola y teclas de la palaa

--- scenes/Game.js
+++ scenes/Game.js
@@ -15,15 +15,37 @@ export default class Game extends Phaser.Scene {
 
   create() {
      //Pala
-     const rectangle = this.add.rectangle(400, 550, 140, 30, 0xffffff);
+     this.rectangle = this.add.rectangle(400, 550, 140, 30, 0xffffff);
      //Pelota
-     const ball = this.add.circle(200,300,20,0xffffff);
+     this.ball = this.add.circle(200,300,20,0xffffff);
  
-     this.physics.add.existing(rectangle);
-     this.physics.add.existing(ball);
+     this.physics.add.existing(this.rectangle);
+     this.rectangle.body.setImmovable(true);
+     this.rectangle.body.setCollideWorldBounds(true);
+     this.physics.add.existing(this.ball);
+     this.ball.body.setCollideWorldBounds(true);
+     this.ball.body.setBounce(1);
+     this.ball.body.setVelocity(200, 200)
+     this.cursor = this.input.keyboard.createCursorKeys();
+
+     this.physics.add.collider(
+      this.rectangle,
+      this.ball,
+      null,
+      null,
+      this
+    )
   }
 
   update() {
-    // update game objects
+    //Movimiento pala
+    const speed = 500;
+    this.rectangle.body.setVelocity(0);
+        if (this.cursor.left.isDown) {
+            this.rectangle.body.setVelocityX(-speed);
+        } else if (this.cursor.right.isDown) {
+            this.rectangle.body.setVelocityX(speed);
+        }
+        //Movimiento pelota
   }
 }
